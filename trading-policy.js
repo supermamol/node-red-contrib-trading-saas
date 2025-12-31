4846e9a02e9aa08987fc5db44a624ee98a0ee8f7
Serge Helly <sergio@localhost-live.home>
2025-12-31 13:41:13 +0100

    systeme pre v1 avec blocage Ticker -> Backtest (policy dans Ticker et non dans un node Policy)

--- trading-policy.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (RED) {
-    function TradingPolicyNode(config) {
-      RED.nodes.createNode(this, config);
-    }
-    RED.nodes.registerType("trading-policy", TradingPolicyNode);
-  };
-  
